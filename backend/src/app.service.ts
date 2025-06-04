13f2d0999420c6a57d692507230a639ad5bed175
unrealmanu <manuel.trebbi@bonusx.it>
2025-06-04 16:46:56 +0200

    ref: controller

--- backend/src/app.service.ts
+++ backend/src/app.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  getHello(): { message: string } {
+    return { message: "I'm Mr. Meeseeks, look at me!" };
   }
 }
