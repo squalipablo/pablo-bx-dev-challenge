75e7644cb4f8798c4b8866b5ddb0cab6c0bab820
unrealmanu <manuel.trebbi@bonusx.it>
2025-06-04 10:50:56 +0200

    feat: jest

--- frontend/rsbuild.config.ts
+++ frontend/rsbuild.config.ts
@@ -3,6 +3,14 @@ import { pluginReact } from "@rsbuild/plugin-react";
 
 export default defineConfig({
   plugins: [pluginReact()],
+  source: {
+    exclude: [
+      /\.spec\.(tsx?|jsx?)$/,
+      /\.test\.(tsx?|jsx?)$/,
+      /__tests__/,
+      /\.stories\.(tsx?|jsx?)$/,
+    ],
+  },
   html: {
     meta: {
       viewport: "initial-scale=1, width=device-width",
